153613d4d7172c6c49c714822c4385d0427f2095
Freedom Loisa <kitakayaloisa@gmail.com>
2021-12-04 16:52:26 +0300

    used regex in url to tage spacific value

--- drinks_app/urls.py
+++ drinks_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls.resolvers import URLPattern
@@ -8,7 +8,8 @@ from .views import *
 # mapping urls
 urlpatterns = [
     path('', base, name='base'),
-    path('drinks', home, name='home'),    
+    path('drinks', home, name='home'),
+    re_path(r'^drinks/(?P<id>\d+)/$', details, name='details')
 ]
 
 handler404 = 'drinks_app.views.error_404'
