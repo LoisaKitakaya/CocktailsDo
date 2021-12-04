80c2d12dcf376922edfcb10da440505d35ff48b2
Freedom Loisa <kitakayaloisa@gmail.com>
2021-12-04 16:51:39 +0300

    created view for details page

--- drinks_app/views.py
+++ drinks_app/views.py
@@ -14,10 +14,22 @@ def home(request):
 
     drinksdb = DrinksData.objects.all()
 
-    print('myoutput', drinksdb)
+    print('Output: ', drinksdb)
 
     context = {
         'drinksdb' : drinksdb
     }
 
     return render(request, 'drinks_app/index.html', context)
+
+def details(request, id):
+
+    drink_details = DrinksData.objects.get(id=id)
+
+    print('Output: ', drink_details)
+
+    context = {
+        'drink_details' : drink_details
+    }
+
+    return render(request, 'drinks_app/details.html', context)
