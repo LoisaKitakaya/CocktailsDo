f656b54e1e92215fe67136795845d3b25bfaf6cd
Freedom Loisa <kitakayaloisa@gmail.com>
2021-12-04 13:40:09 +0300

    added views for 404 page not found

--- drinks_app/views.py
+++ drinks_app/views.py
@@ -5,6 +5,10 @@ def base(request):
 
     return render(request, 'drinks_app/base.html')
 
+def error_404(request, exception):
+
+    return render(request, 'drinks_app/404.html', status=404)
+
 def home(request):
 
     return render(request, 'drinks_app/index.html')
